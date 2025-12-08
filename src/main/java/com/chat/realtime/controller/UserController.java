caceef2c984ad057a403efd8872db902e337c1d2
JorderGomes <jordernoveau@gmail.com>
2025-12-08 17:15:15 -0300

    feat: add user endpoints

---
+++ src/main/java/com/chat/realtime/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.chat.realtime.controller;
+
+import com.chat.realtime.model.User;
+import com.chat.realtime.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userService.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<User>> getUsersByName(@RequestParam String name){
+        List<User> users = userService.findByName(name);
+        return ResponseEntity.ok(users);
+    }
+}
---
+++ src/main/java/com/chat/realtime/service/UserService.java
@@ -0,0 +1,29 @@
+package com.chat.realtime.service;
+
+import com.chat.realtime.model.User;
+import com.chat.realtime.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> findByName(String name){
+        return userRepository.findByName(name);
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+}
